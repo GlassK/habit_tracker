b3662b117687b1590ac7c51df39b2827cfbcc3ce
GlassK <glassk0724@gmail.com>
2021-04-07 18:23:34 +0900

    feat: Add Form 생성

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import Habit from "./habit";
+import HabitAddForm from "./habitAddForm";
 
 class Habits extends Component {
+  handleAdd = (name) => {
+    this.props.onAdd(name);
+  };
+
   render() {
     return (
-      <ul className="habits">
-        {this.props.habits.map((habit) => (
-          <Habit
-            key={habit.id}
-            habit={habit}
-            onIncrement={this.props.onIncrement}
-            onDecrement={this.props.onDecrement}
-            onDelete={this.props.onDelete}
-          />
-        ))}
-      </ul>
+      <>
+        <HabitAddForm onAdd={this.handleAdd} />
+        <ul className="habits">
+          {this.props.habits.map((habit) => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+              //onDelete={(habit) => {this.props.handleDelete(habit);}}
+            />
+          ))}
+        </ul>
+      </>
     );
   }
 }
