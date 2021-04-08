a4f55f29d5ca0ff809b6717a90a1596afdfb33e3
GlassK <glassk0724@gmail.com>
2021-04-08 15:08:32 +0900

    refactor: Habits 불필요한 리렌더링 보완

--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -3,30 +3,41 @@ import Habit from "./habit";
 import HabitAddForm from "./habitAddForm";
 
 class Habits extends Component {
+  handleIncrement = (habit) => {
+    this.props.onIncrement(habit);
+  };
+
+  handleDecrement = (habit) => {
+    this.props.onDecrement(habit);
+  };
+
+  handleDelete = (habit) => {
+    this.props.onDelete(habit);
+  };
+
   handleAdd = (name) => {
     this.props.onAdd(name);
   };
 
   render() {
     return (
-      <>
+      <div className="habits">
         <HabitAddForm onAdd={this.handleAdd} />
-        <ul className="habits">
+        <ul>
           {this.props.habits.map((habit) => (
             <Habit
               key={habit.id}
               habit={habit}
-              onIncrement={this.props.onIncrement}
-              onDecrement={this.props.onDecrement}
-              onDelete={this.props.onDelete}
-              //onDelete={(habit) => {this.props.handleDelete(habit);}}
+              onIncrement={this.handleIncrement}
+              onDecrement={this.handleDecrement}
+              onDelete={this.handleDelete}
             />
           ))}
         </ul>
         <button className="habits-reset" onClick={this.props.onReset}>
           Reset All
         </button>
-      </>
+      </div>
     );
   }
 }
